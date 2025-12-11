6c6feb61f0709fe0ebdbe05938eabe42e58664eb
Bob Verheij <bob.verheij@nn-group.com>
2025-12-11 09:23:32 +0100

    finished day 11

--- src/2025/11/01.ts
+++ src/2025/11/01.ts
@@ -1,5 +1,7 @@
 import { readFileSync } from "fs";
 
+import { Memo } from "../../../utils/memoize";
+
 export const getInput = (file: string) => {
   const fileContent = readFileSync(file, "utf-8");
   return fileContent.split("\n");
@@ -7,5 +9,45 @@ export const getInput = (file: string) => {
 
 export const solution = async (file: string): Promise<string | number> => {
   const input = getInput(file);
-  return "";
+
+  const devices = input.map((l) => {
+    const splitted = l.split(": ");
+    const index = splitted[0];
+    const connections = splitted[1].split(" ").map((c) => c.trim());
+    const isExit = connections.includes("out");
+    return {
+      index,
+      connections,
+      isExit,
+    };
+  });
+
+  const memo = new Memo("exitscheck");
+
+  const checkExitLeadsToYou: (index: string) => number = memo.memoize(
+    (index: string) => {
+      if (index === "you") {
+        return 1;
+      }
+      const targets = devices.filter((d) => d.connections.includes(index));
+
+      if (targets.length) {
+        return targets
+          .map((t) => checkExitLeadsToYou(t.index))
+          .reduce((acc, cur) => (acc += cur), 0);
+      }
+      return 0;
+    }
+  );
+
+  return devices
+    .filter((d) => d.isExit)
+    .map((d) => {
+      const goesToYou = checkExitLeadsToYou(d.index);
+      console.log(d, goesToYou);
+      return goesToYou;
+    })
+    .reduce((acc, cur) => {
+      return (acc += cur);
+    }, 0);
 };
--- src/2025/11/02.ts
+++ src/2025/11/02.ts
@@ -1,6 +1,68 @@
+import { Memo } from "../../../utils/memoize";
 import * as prior from "./01";
 
 export const solution = async (file: string): Promise<string | number> => {
   const input = prior.getInput(file);
-  return "";
+
+  const devices = input.map((l) => {
+    const splitted = l.split(": ");
+    const index = splitted[0];
+    const connections = splitted[1].split(" ").map((c) => c.trim());
+    const isExit = connections.includes("out");
+    return {
+      index,
+      connections,
+      isExit,
+    };
+  });
+
+  const memo = new Memo("exitscheck");
+
+  const checkExitLeadsToYou: (
+    index: string,
+    dac: boolean,
+    fft: boolean
+  ) => number = memo.memoize((index: string, dac: boolean, fft: boolean) => {
+    let passedThrough = 0;
+    if (dac) {
+      passedThrough += 1;
+    }
+    if (fft) {
+      passedThrough += 1;
+    }
+
+    if (index === "svr" && dac && fft) {
+      return 1;
+    }
+
+    const targets = devices.filter((d) => d.connections.includes(index));
+
+    if (targets.length) {
+      return targets
+        .map((t) =>
+          checkExitLeadsToYou(
+            t.index,
+            dac || index === "dac",
+            fft || index === "fft"
+          )
+        )
+        .reduce((acc, cur) => (acc += cur), 0);
+    }
+    return 0;
+  });
+
+  return devices
+    .filter((d) => d.isExit)
+    .map((d) => {
+      const goesToYou = checkExitLeadsToYou(
+        d.index,
+        d.index === "dac",
+        d.index === "fft"
+      );
+      console.log(d, goesToYou);
+      return goesToYou;
+    })
+    .reduce((acc, cur) => {
+      return (acc += cur);
+    }, 0);
 };
